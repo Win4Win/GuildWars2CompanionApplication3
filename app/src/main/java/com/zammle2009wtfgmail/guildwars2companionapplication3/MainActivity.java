3d03bf05e5018f1bec94620a0bedff701ef07301
Jake Lewis <zammle2009wtf@gmail.com>
2019-02-05 13:25:40 -0500

    4th commit

--- app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/MainActivity.java
+++ app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/MainActivity.java
@@ -2,13 +2,17 @@ package com.zammle2009wtfgmail.guildwars2companionapplication3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
 
