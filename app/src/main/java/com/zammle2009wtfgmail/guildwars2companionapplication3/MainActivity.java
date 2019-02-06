31777bd552fa1f2b15cbf59320e273e459ef9de7
Jake Lewis <zammle2009wtf@gmail.com>
2019-02-05 23:24:54 -0500

    Every button and image now has a "final" connected to them. All buttons are hooked up and can now do things when pressed. Companion UI page completed. Still need to implement code to do functions. No bugs as of yet. Have not tested for multiple devices.

--- app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/MainActivity.java
+++ app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
@@ -87,6 +88,150 @@ public class MainActivity extends AppCompatActivity {
                 temp2.setBackgroundResource(R.drawable.selected2);
             }
 
+
+        });
+
+
+
+        final Button companion = (Button) findViewById(R.id.companion);
+        companion.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                Intent history = new Intent (MainActivity.this, History.class );
+                startActivity(history);
+                 **/
+            }
+
+        });
+
+
+        final Button hero = (Button) findViewById(R.id.heroPanel);
+        hero.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final Button forums = (Button) findViewById(R.id.forums);
+        forums.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final Button WvW = (Button) findViewById(R.id.wvw);
+        WvW.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final Button more = (Button) findViewById(R.id.more);
+        more.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+
+        final ImageButton news1 = (ImageButton) findViewById(R.id.news1);
+        news1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final ImageButton news2 = (ImageButton) findViewById(R.id.news2);
+        news2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final ImageButton news3 = (ImageButton) findViewById(R.id.news3);
+        news3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final ImageButton news4 = (ImageButton) findViewById(R.id.news4);
+        news4.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
+
+        });
+
+        final ImageButton news5 = (ImageButton) findViewById(R.id.news5);
+        news1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /**
+                 Intent history = new Intent (MainActivity.this, History.class );
+                 startActivity(history);
+                 **/
+            }
 
         });
 
