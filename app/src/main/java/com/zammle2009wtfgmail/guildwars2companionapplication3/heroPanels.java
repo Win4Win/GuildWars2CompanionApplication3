d908fbcde1bd046d8abffcc3c5b1097bf9885937
Jake Lewis <zammle2009wtf@gmail.com>
2019-02-21 13:33:11 -0500

    CLEAN BUILD NO CRASHES

--- app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/heroPanels.java
+++ app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/heroPanels.java
@@ -1,48 +1,15 @@
 package com.zammle2009wtfgmail.guildwars2companionapplication3;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-
 public class heroPanels extends AppCompatActivity {
 
-
-
-    static ArrayList<itemHolder> Holder = new ArrayList<>();
-    private RecyclerView mWeapon;
-    private RecyclerView mArmor;
-    private RecyclerView mTrinket;
-    private RecyclerView mAqua;
-    private RecyclerView mGather;
-
-    private RecyclerView.LayoutManager weaponLayout;
-    private RecyclerView.LayoutManager armorLayout;
-    private RecyclerView.LayoutManager trinketLayout;
-    private RecyclerView.LayoutManager aquaLayout;
-    private RecyclerView.LayoutManager gatherLayout;
-
-
-
-    private itemAdapter mWeaponAdapter;
-    private itemAdapter mArmorAdapter;
-    private itemAdapter mTrinketAdapter;
-    private itemAdapter mAquaAdapter;
-    private itemAdapter mGatherAdapter;
 
 
     @Override
@@ -172,8 +139,8 @@ public class heroPanels extends AppCompatActivity {
             public void onClick(View v)
             {
 
-                 Intent history = new Intent (heroPanels.this, MainActivity.class );
-                 startActivity(history);
+                Intent history = new Intent (heroPanels.this, MainActivity.class );
+                startActivity(history);
 
             }
 
@@ -215,8 +182,8 @@ public class heroPanels extends AppCompatActivity {
             public void onClick(View v)
             {
 
-                 Intent history = new Intent (heroPanels.this, worldVsWorld.class );
-                 startActivity(history);
+                Intent history = new Intent (heroPanels.this, worldVsWorld.class );
+                startActivity(history);
 
             }
 
@@ -238,87 +205,5 @@ public class heroPanels extends AppCompatActivity {
 
 
 
-        /////////////////////////////////////
-        /////// SETTING UP ADAPTES //////////
-        /////////////////////////////////////
-
-        Holder.clear();
-
-
-
-
-        for (int i = 0; i < 4; i = ++i)
-        {
-
-
-                String itemName = "ItemNameLOL";
-                String itemType = "ItemTypeLOL";
-
-                Drawable item;
-                Drawable infusion1;
-                Drawable infusion2;
-                Drawable infusion3;
-
-
-                try
-                {
-                    item.
-                }
-
-                try {
-
-                    Drawable icon = getPackageManager().getApplicationIcon(TextWithInfo[i+3]);
-
-
-
-                        Holder.add(new itemHolder(icon,appName, bool, Time, true,PackageName));
-
-                }
-                catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-
-
-
-                }
-
-        }
-
-
-
-
-
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-
-
-
-        mWeapon = findViewById(R.id.weaponrecycle);
-        mWeapon.setHasFixedSize(true);
-        weaponLayout = new LinearLayoutManager(this);
-        mWeaponAdapter = new itemAdapter(Holder);
-
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mWeapon.getContext(), layoutManager.getOrientation());
-
-        mWeapon.addItemDecoration(dividerItemDecoration);
-
-
-        mWeapon.setLayoutManager(weaponLayout);
-        mWeapon.setAdapter(mWeaponAdapter);
-
-
-
-
-
-
-
-
-
-
 
     }}
-
-
-
-
-
-
--- app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/testPage.java
+++ app/src/main/java/com/zammle2009wtfgmail/guildwars2companionapplication3/testPage.java
@@ -2,17 +2,22 @@ package com.zammle2009wtfgmail.guildwars2companionapplication3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -22,16 +27,44 @@ public class testPage extends AppCompatActivity {
 
     static String urls2="";
 
-        public static TextView testpage;
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity);
 
-        testpage = (TextView) findViewById(R.id.info1);
-        fetchData process = new fetchData();
-        process.execute();
+
+
+
+
+
+
+
+
+
+        new DownloadImageTask((ImageView) findViewById(R.id.testview))
+                .execute("https://render.guildwars2.com/file/D5A5A4083E97C4EF0226315226E610C01DEE2248/1423739.png");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
